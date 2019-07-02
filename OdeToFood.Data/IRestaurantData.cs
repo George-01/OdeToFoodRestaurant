bb8d07ee322d49a583763a8391f84d0ece689024
George <george.coker@hotmail.com>
2019-07-02 15:26:30 +0100

    Implemented Details page successfully.

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace OdeToFood.Data
    public  interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name );
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -24,6 +25,12 @@ namespace OdeToFood.Data
                 new Restaurant{Id = 3, Name = "La costa", Location =  "California", Cuisine = CuisineType.Mexican}
             };
         }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
